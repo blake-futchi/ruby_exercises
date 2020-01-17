d53cec983427b84ef401c9fc38b1f4571da02e2f
unknown <blake@futchi.com>
2020-01-17 11:31:37 -0800

    First commit ruby movie release date exercise

---
+++ movies.rb
@@ -0,0 +1,11 @@
+movie1 = {name: "Tron Legacy", year: "2010"}
+movie2 = {name: "Gladiator", year: "2000"}
+movie3 = {name: "Avengers: EndGame", year: "2018"}
+movie4 = {name: "Modern Times", year: "1936"}
+movie5 = {name: "Black Panther", year: "2018"}
+
+puts " #{movie1[:name]}"
+puts " #{movie2[:name]}"
+puts " #{movie3[:name]}"
+puts " #{movie4[:name]}"
+puts " #{movie5[:name]}"
--- output_name.rb
+++ output_name.rb
@@ -2,4 +2,4 @@ puts "Hey you! What's you're first name?"
 firstname = gets.chomp
 puts "Righteous mate, now give me the last name."
 lastname = gets.chomp
-puts ("Hey #{firstname} #{lastname}!")
+puts ("Aloha #{firstname} #{lastname}!")
