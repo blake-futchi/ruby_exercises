03292c16e23fae35d342f63a0c30cab7b8d60df8
unknown <blake@futchi.com>
2020-01-17 11:59:03 -0800

    Adding first hash exercise

--- age.rb
+++ age.rb
@@ -4,6 +4,6 @@ inputAge = gets.chomp.to_i
 ages = [10,20,30,40]
 
 ages.each do |age|
-    tempage = age + inputAge
-    puts "In #{age} years you are going to be: #{tempage} years old."
+    futureage = age + inputAge
+    puts "In #{age} years you are going to be: #{futureage} years old."
 end
