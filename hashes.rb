73242095e0e822a572143f509853b5c0439e8ad6
unknown <blake@futchi.com>
2020-01-22 10:57:44 -0800

    added exercise to hash file

--- hashes.rb
+++ hashes.rb
@@ -1,3 +1,4 @@
+#get sisters and brothers from hash
 family = {
             uncles: ["bob", "joe" ,"steve"],
             sisters: ["jane", "jill" ,"beth"],
@@ -9,3 +10,7 @@ immediate_family = family.select do |key, value|
         [:sisters,:brothers].include? key
 end
 puts immediate_family
+
+#get name of person
+person = {name: 'Bob', occupation: 'web developer', hobbies: 'painting'}
+puts person[:name]
